c53249e1908daa7b234ce302be75b5f5bf9fa4f9
geralt <geralt@fixmost.com>
2021-09-20 17:29:57 +0300

    HW3 done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,73 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"strings"
+)
+
+type TopWord struct {
+	count int
+	word  string
+}
+
+func (c *TopWord) Initialize() {
+	c.count = 0
+	c.word = ""
+}
+
+func (c *TopWord) Check(word string, count int) {
+	if count > c.count {
+		c.count = count
+		c.word = word
+	}
+
+	if count == c.count && strings.Compare(c.word, word) == 1 {
+		c.word = word
+	}
+}
+
+func (c *TopWord) String() string {
+	return c.word
+}
+
+func Top10(s string) []string {
+	var result []string
+
+	words := getWordsWithCount(s)
+	if len(words) == 0 {
+		return result
+	}
+
+	var curr TopWord
+
+	for len(result) < 10 && len(words) > 0 {
+		curr.Initialize()
+
+		for word, count := range words {
+			curr.Check(word, count)
+		}
+
+		result = append(result, curr.String())
+		delete(words, curr.String())
+	}
+
+	return result
+}
+
+func getWordsWithCount(s string) map[string]int {
+	result := make(map[string]int)
+
+	r := regexp.MustCompile(`[^A-zА-яеЁ]*([A-zА-я]+[A-zА-я-]*)[^A-zА-яеЁ]*`)
+
+	for _, w := range strings.Fields(s) {
+		if !r.MatchString(w) {
+			continue
+		}
+
+		w = r.ReplaceAllString(w, "$1")
+
+		result[strings.ToLower(w)]++
+	}
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -78,5 +78,13 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("less than 10 words", func(t *testing.T) {
+		require.Len(t, Top10("   hello! my     name is     Vova!   !!!"), 5)
+	})
+
+	t.Run("only symbols", func(t *testing.T) {
+		require.Len(t, Top10("! , - . > < # @ $ % & * ?"), 0)
 	})
 }
